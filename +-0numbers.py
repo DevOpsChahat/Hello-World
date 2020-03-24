4d0af065e12612087ec0290e065d3d4eca9bd16e
DevOpsChahat <53637470+DevOpsChahat@users.noreply.github.com>
2020-03-24 18:01:43 +0530

    Add files via upload

---
+++ +-0numbers.py
@@ -0,0 +1,10 @@
+number=float(input("Enter a number"))
+if(number==0):
+    print("Number is zero")
+elif(number>0):
+    print("Number is positive")
+elif(number<0):
+    print("Number is negative")
+else:
+    ("I don't know")
+             
